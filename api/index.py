24bebf612ba42a17e85ea3fc5e5d6f8d46e132df
MicaelSv <micaelsilva012@hotmail.com>
2025-05-08 19:15:53 -0300

    erro-cors

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware 
 
 from .database import engine, Base
 from api.routes import participante, publicacao
@@ -6,6 +7,14 @@ from api.routes import participante, publicacao
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  #["http://localhost:3000"] para limitar ao front
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(participante.router, prefix="/participantes", tags=["Participantes"])
 app.include_router(publicacao.router, prefix="/publicacoes", tags=["Publicações"])
