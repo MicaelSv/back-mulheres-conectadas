c4129cd7d8dd93b67317af190396cd101d624180
MicaelSv <micaelsilva012@hotmail.com>
2025-05-14 18:12:48 -0300

    bd-mudanca2

--- api/database.py
+++ api/database.py
@@ -5,7 +5,7 @@ import os
 
 #DATABASE_URL = "postgresql://neondb_owner:npg_cYOH9bC7mKda@ep-falling-base-acq4r8c0-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require"
 
-DATABASE_URL = "postgresql://postgres:OuYYQdwFSBWFDihDyWELJaiUzfzvKfko@postgres.railway.internal:5432/railway"
+DATABASE_URL = "postgresql://postgres:OuYYQdwFSBWFDihDyWELJaiUzfzvKfko@turntable.proxy.rlwy.net:26001/railway"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
